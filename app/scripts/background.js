90f76b993ad8021c72b3b94dd1fd8b33c864c220
Louis Prud'homme <louis.prudhomme@hotmail.com>
2021-07-17 15:49:01 +0100

    Removed useless code in background file Refactored

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,9 @@
-browser.runtime.onInstalled.addListener((details) => {
+/*browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
 browser.browserAction.setBadgeText({
   text: `'Allo`
-})
+})*/
 
-console.log(`'Allo 'Allo! Event Page for Browser Action`)
+//console.log(`'Allo 'Allo! Event Page for Browser Action`)
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,11 @@
-console.log(`'Allo 'Allo! Popup`)
+import init, { check_db_exists, Cred, sign_up } from "keyring"; 
+
+const launchBackend = async () => {
+    await init();
+    
+    console.log(check_db_exists());
+    console.log(sign_up(Cred.new("test_login", "test_pass")));
+    console.log(check_db_exists());   
+}
+
+launchBackend();
