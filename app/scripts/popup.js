d4f1d0d8eb2b091a640f4cc75ecf44d657e85e03
Louis Prud'homme <louis.prudhomme@hotmail.com>
2021-07-22 09:42:42 +0100

    Refined UI

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,27 +1,170 @@
-import init, { check_db_exists, Cred, create_cred, obtain_cred, sign_up, sign_in } from "keyring"; 
+import init, { check_db_exists, Cred, create_cred, obtain_cred, sign_up, sign_in } from "keyring";
 
-const launchBackend = async () => {
+async function launchBackend() {
+    signForm.setAttribute("hidden", true);
+    createForm.setAttribute("hidden", true);
+    obtainForm.setAttribute("hidden", true);
+    errorPrompt.setAttribute("hidden", true);
+    successPrompt.setAttribute("hidden", true);
+    obtainPass.setAttribute("hidden", true);
+
+    mainPrompt.removeAttribute("hidden");
     await init();
-    
-    console.clear();
-    localStorage.clear();
-
-    console.log(check_db_exists());
-    console.log(sign_up(Cred.from("test_login", "test_pass")));
-    //console.log(check_db_exists());
-    //console.log(sign_in(Cred.new("test_login", "test_pass")));
-    //console.log(sign_in(Cred.new("wrong", "wrong")));
-
-    console.log(create_cred(
-        Cred.from("test_login", "test_pass"),
-        Cred.from("test_cred", "test_cred")
-    ));
-
-    let ptr = obtain_cred(Cred.from("test_login", "test_pass"), 
-        "test_cred");
-    console.log(ptr);
-    let cred = new Cred(ptr.get());
-    console.log(cred);
+    mainPrompt.setAttribute("hidden", true);
 }
 
-launchBackend();
+async function initUi() {
+    configAliases();
+    await launchBackend();
+
+    const isDb = check_db_exists();
+
+    if (isDb) {
+        signPrompt.textContent = "Sign in";
+        signBtn.onclick = () => signin();
+    } else {
+        signPrompt.textContent = "Sign up";
+        signBtn.onclick = () => signup();
+    }
+
+    reinitBtn.onclick = () => deleteDb();
+    createBtn.onclick = () => createCred();
+    obtainBtn.onclick = () => obtainCred();
+
+    signForm.removeAttribute("hidden");
+}
+
+function deleteDb() {
+    console.log("Erased database");
+    localStorage.clear();
+    window.close();
+}
+
+async function createCred() {
+    await launchBackend();
+
+    let isOk
+
+    try { isOk = create_cred(Cred.from(signLogin.value, signPass.value), Cred.from(createLogin.value, createPass.value)); }
+    catch (e) { errorDisplay(isOk); }
+
+    alternateForms(!!isOk);
+
+    if (isOk) {
+        createLogin.value = "";
+        createPass.value = "";
+        successPrompt.textContent = "Credential successfully created."
+        successPrompt.removeAttribute("hidden");
+    }
+}
+
+function alternateForms(isSignHidden) {
+    if (isSignHidden) {
+        signForm.setAttribute("hidden", true);
+
+        createForm.removeAttribute("hidden");
+        obtainForm.removeAttribute("hidden");
+    } else {
+        signForm.removeAttribute("hidden");
+
+        createForm.setAttribute("hidden", true);
+        obtainForm.setAttribute("hidden", true);
+    }
+
+}
+
+function errorDisplay(error) {
+    errorPrompt.textContent = error;
+
+    errorPrompt.removeAttribute("hidden");
+
+    console.error(error.stack);
+}
+
+async function obtainCred() {
+    await launchBackend();
+
+    let pass
+
+    try { pass = obtain_cred(Cred.from(signLogin.value, signPass.value), obtainLogin.value); }
+    catch (e) { errorDisplay(pass); }
+
+    if (!!pass) {
+        obtainPass.value = pass;
+        obtainPass.removeAttribute("hidden");
+    }
+
+    alternateForms(true);
+}
+
+async function signin() {
+    await launchBackend();
+
+    let isOk
+    try { isOk = sign_in(Cred.from(signLogin.value, signPass.value)); }
+    catch (e) { errorDisplay(isOk); }
+
+    if (!isOk) {
+        errorDisplay("Wrong credentials");
+    }
+
+    alternateForms(!!isOk);
+}
+
+async function signup() {
+    await launchBackend();
+
+    let isOk
+    try { isOk = sign_up(Cred.from(signLogin.value, signPass.value)); }
+    catch (e) { errorDisplay(isOk); }
+
+    alternateForms(!!isOk);
+}
+
+window.onload = () => { console.clear(); initUi().then(() => { }); };
+
+let signForm;
+let signPrompt;
+let signLogin;
+let signPass;
+let signBtn;
+
+let obtainForm;
+let obtainLogin;
+let obtainPass;
+let obtainBtn;
+
+let createForm;
+let createLogin;
+let createPass;
+let createBtn;
+
+let mainPrompt;
+let successPrompt;
+let errorPrompt;
+let errorMessage;
+let reinitBtn;
+
+function configAliases() {
+    mainPrompt = document.getElementById("id_main_prompt");
+    successPrompt = document.getElementById("id_success_prompt");
+    errorPrompt = document.getElementById("id_error_prompt");
+    errorMessage = document.getElementById("id_error_message");
+    reinitBtn = document.getElementById("id_reinit_btn");
+
+    signForm = document.getElementById("id_sign_form");
+    signPrompt = document.getElementById("id_sign_prompt");
+    signLogin = document.getElementById('id_sign_login');
+    signPass = document.getElementById('id_sign_pass');
+    signBtn = document.getElementById('id_sign_btn');
+
+    createForm = document.getElementById('id_create_form');
+    createLogin = document.getElementById('id_create_login');
+    createPass = document.getElementById('id_create_pass');
+    createBtn = document.getElementById('id_create_btn');
+
+    obtainForm = document.getElementById('id_obtain_form');
+    obtainLogin = document.getElementById('id_obtain_login');
+    obtainPass = document.getElementById('id_obtain_pass');
+    obtainBtn = document.getElementById('id_obtain_btn');
+}
